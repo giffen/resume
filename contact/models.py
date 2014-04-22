70ea8eb3c9f2046541dfccc3e5383549d0a49519
Andy Giffen <giffen@gmail.com>
2014-04-21 17:56:19 -0700

    Update contact model

--- contact/models.py
+++ contact/models.py
@@ -2,10 +2,9 @@ from django.db import models
 from django.utils.encoding import smart_unicode
 
 class Contact(models.Model):
-	first_name = models.CharField(max_length=120, null=True, blank=True)
-	last_name = models.CharField(max_length=120, null=True, blank=True)
+	full_name = models.CharField(max_length=200, null=True, blank=False)
 	email = models.EmailField(null=False, blank=False)
-	message = models.CharField(max_length=500, null=True, blank=True)
+	message = models.CharField(max_length=600, null=True, blank=False)
 	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
 	update = models.DateTimeField(auto_now_add=False, auto_now=True)
 
